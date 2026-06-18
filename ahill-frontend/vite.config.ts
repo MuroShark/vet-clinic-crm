ace8f6c5519e8e59790af52d4407fc4556874a2d
MuroShark <shgefan.zhora@gmail.com>
2026-06-18 20:37:55 +0300

    fix: revert base path to relative

--- ahill-frontend/vite.config.ts
+++ ahill-frontend/vite.config.ts
@@ -3,6 +3,6 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	base: '/vet-clinic-crm/',
+	base: './',
 	plugins: [tailwindcss(), sveltekit()]
 });
